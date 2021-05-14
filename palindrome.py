63e00b1716e03f5c1e7d07b4bffb9bbdeafaed30
ReejutaBhattacharya <84181992+ReejutaBhattacharya@users.noreply.github.com>
2021-05-14 16:53:25 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 16:03:09 2021
+
+@author: Debashis
+"""
+
+
+s=input()
+j=len(s)-1
+i=0
+flag=0
+while(i<j):
+    if(s[i]!=s[j]):
+        flag=1
+        break
+    else:
+        i+=1
+        j-=1
+if(flag==0):
+    print("pallindrome")
+else:
+    print("not pallindrome")
